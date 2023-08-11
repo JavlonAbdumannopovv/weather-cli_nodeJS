28c71dabbcdec05b273122a2e5da69c3a93ae1b1
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-08-11 21:09:46 +0500

    Save Token

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-import  getArgs from "./helpers/args.js";
-import {printError, printSuccess, printHelp} from "./services/log.service.js";
+import getArgs from "./helpers/args.js";
+import { printError, printSuccess, printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token was saved");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const startCli = () => {
   const args = getArgs(process.argv);
-
   if (args.h) {
     //help
     printHelp();
   }
-
   if (args.s) {
     //save city
   }
-
   if (args.t) {
     //save token
+    return saveToken(args.t);
   }
 
   //result
-}
+};
 
 startCli();
